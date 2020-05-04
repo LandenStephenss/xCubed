704b53993acea1ef52f0d55f800964ffb6731834
LandenStephenss <turtlederp806@icloud.com>
2020-05-04 15:53:15 -0500

    mobile indicator > *

--- src/Assets/Structures/Client.js
+++ src/Assets/Structures/Client.js
@@ -2,17 +2,20 @@ const {
 	Client,
 	Collection
 } = require('discord.js');
-const { MongoClient } = require('mongodb');
+require("discord.js/src/util/Constants").DefaultOptions.ws.properties.$browser = "Discord iOS"
+const {
+	MongoClient
+} = require('mongodb');
 class xCubed extends Client {
 	constructor(options = {}) {
 		super();
-		if(process.argv.includes('--dev')) {
+		if (process.argv.includes('--dev')) {
 			this.config = require('../Configs/devConfig.json');
 		} else {
 			this.config = require('../Configs/prodConfig.json');
 		}
-		MongoClient.connect(`mongodb://${this.config.DatabaseUserName}:${this.config.DatabasePwd}@${this.config.DatabaseIP}:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`, (err, data)  => {
-			if(err) {
+		MongoClient.connect(`mongodb://${this.config.DatabaseUserName}:${this.config.DatabasePwd}@${this.config.DatabaseIP}:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`, (err, data) => {
+			if (err) {
 				console.log(err);
 			}
 			this.userDB = data.db(this.config.Database).collection('users');
@@ -22,7 +25,6 @@ class xCubed extends Client {
 		this.aliases = new Collection();
 		this.commandsUsed = 0;
 	}
-
 }
 
 module.exports = xCubed;
